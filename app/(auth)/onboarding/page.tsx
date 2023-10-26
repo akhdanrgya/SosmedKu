566da3fd1b482414c0527acf6a4750eb3462306e
akhdanrgya <akhdan.anargya@gmail.com>
2023-10-26 20:15:05 +0700

    fixing profile

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,19 +1,23 @@
 import AccountProfile from "@/components/forms/AccountProfile"
 import { currentUser } from "@clerk/nextjs"
+import { fetchUser } from "@/lib/actions/user.action";
+import { redirect } from "next/navigation";
 
 async function page() {
     const user = await currentUser()
+    if (!user) return null
 
-    const userInfo = {}
+    const userInfo = await fetchUser(user.id);
+    if (userInfo?.onboarded) redirect("/");
 
     const userData = {
-        id: user?.id,
-        objectId:  userInfo?._id,
-        username: userInfo?.username || user?.username,
-        name: userInfo?.name || user.firstName || "",
-        bio: userInfo?.bio || "",
-        image: userInfo?.image || user.imageUrl,
-    }
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
+  };
 
     return (
         <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
