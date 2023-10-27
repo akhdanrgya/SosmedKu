0e16615c964c5d3403c66fd396f4005bba44bcbd
akhdanrgya <akhdan.anargya@gmail.com>
2023-10-27 11:35:57 +0700

    Update Activities Feature

--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -108,37 +108,41 @@ export async function fetchThreadById(id: string) {
   }
 }
 
-export async function addCommentToThread(threadId: string, commentText: string, userId: string, path: string){
-    connectToDb()
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) {
+  connectToDb();
 
+  try {
+    // Find the original thread by its ID
+    const originalThread = await Thread.findById(threadId);
 
-    try {
-        // mencari original thread berdasarkan id
-        const originalThread = await Thread.findById(threadId)
-
-        if(!originalThread){
-            throw new Error('Thread not found')
-        }
-
-
-        // Membuat thread baru dengan comment text
-        const commentThread = new Thread({
-            text: commentText,
-            author: userId,
-            parentId: threadId
-        })
-
-        //menyimpan thread baru
-        const savedCommentThread = await commentThread.save()
-
-        // untuk update original thread termasuk comment barunya anjay mabar gua pusing
-        originalThread.children.push(savedCommentThread._id)
-
-        // menyimpan original thread
-        await originalThread.save()
-
-        revalidatePath(path)
-    } catch (error: any) {
-        throw Error(`Error adding comment to thread: ${error.message}`)
+    if (!originalThread) {
+      throw new Error("Thread not found");
     }
-};
+
+    // Create the new comment thread
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId, // Set the parentId to the original thread's ID
+    });
+
+    // Save the comment thread to the database
+    const savedCommentThread = await commentThread.save();
+
+    // Add the comment thread's ID to the original thread's children array
+    originalThread.children.push(savedCommentThread._id);
+
+    // Save the updated original thread to the database
+    await originalThread.save();
+
+    revalidatePath(path);
+  } catch (err) {
+    console.error("Error while adding comment:", err);
+    throw new Error("Unable to add comment");
+  }
+}
